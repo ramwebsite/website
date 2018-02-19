8e6d39c80ab4d013b4c14efbb36ad0b326bdea3a
totallyram <36642407+totallyram@users.noreply.github.com>
2018-02-20 01:05:35 +0530

    date change

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 14, 2019 18:03:26",
+	date: "February 21, 2018 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
