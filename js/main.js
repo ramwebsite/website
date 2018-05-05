081657f96384267d3d73cff9f2e1e8e3c2c6901f
totallyram <36642407+totallyram@users.noreply.github.com>
2018-05-05 11:59:40 +0530

    newdate

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "April 10, 2018 00:00:00",
+	date: "June 10, 2018 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
